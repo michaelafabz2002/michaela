0b03f73f3d17793e2f373c7c43d23587c1923c97
michaelafabz2002 <kylafabrigar2002@gmail.com>
2025-07-08 18:51:55 +0800

    final na

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './Contact.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Contact = ({ id, activeSection }) => {
   const isActive = activeSection === id;
@@ -11,7 +13,11 @@ const Contact = ({ id, activeSection }) => {
         <p><strong>Email:</strong> fabrigarmichaela1@gmail.com</p>
         <p><strong>Address:</strong> 423 Cabugao Sur Pavia</p>
       </div>
-      
+      <div className={styles['contact-social-icons']}>
+        <a href="https://www.facebook.com/fabrigar.michaela" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebookF} /></a>
+        <a href="https://www.instagram.com/itsme._.kyla/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
+        <a href="https://github.com/michaelafabz2002" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+      </div>
     </section>
   );
 };
